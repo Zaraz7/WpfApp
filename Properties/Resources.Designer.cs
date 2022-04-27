650dff1c483d61a025c9ca6a6fd0de951e96e992
Maxim Terlik <zaraz121801@gmail.com>
2022-04-27 13:55:08 +0300

    Добавление картинки

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace WpfApp.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _2021_04_04_06_28_58 {
+            get {
+                object obj = ResourceManager.GetObject("_2021_04_04_06_28_58", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
