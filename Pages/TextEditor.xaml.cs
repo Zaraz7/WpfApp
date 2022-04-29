f63c3bf0a71f50f850b73e1f01bccf07ca57fbaa
Maxim Terlik <zaraz121801@gmail.com>
2022-04-29 11:41:56 +0300

    Редактор почти готов (не может работать с многострочными файломи)

--- Pages/TextEditor.xaml.cs
+++ Pages/TextEditor.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using System.Diagnostics;
 
 namespace WpfApp.Pages
 {
@@ -19,9 +22,43 @@ namespace WpfApp.Pages
     /// </summary>
     public partial class TextEditor : Window
     {
+        private OpenFileDialog _openDialog = new OpenFileDialog();
+        private SaveFileDialog _saveAsDialog = new SaveFileDialog();
         public TextEditor()
         {
             InitializeComponent();
+        }
+
+        private void clkOpen(object sender, RoutedEventArgs e)
+        {
+            if (_openDialog.ShowDialog() == true)
+            {
+                StreamReader reader = new StreamReader(_openDialog.FileName);
+                tbEditor.Text = reader.ReadToEnd();
+                reader.Close();
+                _saveAsDialog.FileName = _openDialog.FileName;
+            }
+        }
+        private void clkSaveAs(object sender, RoutedEventArgs e)
+        {
+            if (_saveAsDialog.ShowDialog() == true)
+            {
+                StreamWriter writer = new StreamWriter(_saveAsDialog.FileName);
+                writer.WriteLine(tbEditor.Text);
+                writer.Close();
+            }
+        }
+        private void clkSave(object sender, RoutedEventArgs e)
+        {
+            if (_saveAsDialog.FileName == "") { 
+                clkSaveAs(sender, e);
+                return;
+            }
+
+            StreamWriter writer = new StreamWriter(_saveAsDialog.FileName);
+            writer.WriteLine(tbEditor.Text);
+            writer.Close();
+
         }
     }
 }
